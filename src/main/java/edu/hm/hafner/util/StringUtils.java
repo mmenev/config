9b0e04a28cdd24dec5e5a6965cc226b3ecb4cefe
mmenev <menev@hm.edu>
2013-04-19 16:44:38 +0200

    isSecure method and the tests for it implemented.

--- src/main/java/edu/hm/hafner/util/StringUtils.java
+++ src/main/java/edu/hm/hafner/util/StringUtils.java
@@ -1,5 +1,8 @@
 package edu.hm.hafner.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Several useful utility methods that work on {@link String} instances.
  *
@@ -32,6 +35,7 @@ public final class StringUtils {
      * @param input the input string
      * @param toBeRemoved the string that should be removed
      * @return the stripped string
+     * @author Mihail Menev, menev@hm.edu
      */
     public static String strip(final String input, final String toBeRemoved) {
         String tmp = input;
@@ -39,7 +43,7 @@ public final class StringUtils {
             throw new IllegalArgumentException("None of the parameters should be null");
         }
 
-        if (toBeRemoved.length() == 0) {
+        else if (toBeRemoved.length() == 0) {
             return input;
         }
         else {
@@ -48,6 +52,45 @@ public final class StringUtils {
                 tmp = tmp.replaceAll(String.valueOf(character), "");
             }
             return tmp;
+        }
+    }
+
+    /**
+     * Tests if the password given as parameter is secure enough. A secure
+     * password means- at least 20 charachter- from them:
+     *          - at least 10 unique
+     *          - at least 1 upper case character
+     *          - at least 1 lower case character
+     *          - at least 1 digit
+     *          - at least 1 special character.
+     * @param inputPass the password to be checked
+     * @return true if the password is secure, and false if not
+     * @author Mihail Menev, menev@hm.edu
+     */
+    public static boolean isSecure(final String inputPass) {
+        // Null is valid but weak password
+        if (inputPass == null) {
+            return false;
+        }
+        // Funny way to eliminate the duplicates if any.
+        // Later used to check how many unique characters have the password.
+        final Set<Character> inputPassSet = new HashSet<Character>();
+        char[] inputPassChars = inputPass.toCharArray();
+        for (int i = 0; i < inputPassChars.length; i++) {
+            inputPassSet.add(Character.valueOf(inputPassChars[i]));
+        }
+        // Regexes that match one upper case, one lower case, a number and a special character
+        String upCase = "[\\s\\S]*[A-Z][\\s\\S]*";
+        String lowCase = "[\\s\\S]*[a-z][\\s\\S]*";
+        String digit = "[\\s\\S]*[0-9][\\s\\S]*";
+        String specialChar = "[\\s\\S]*[^A-Za-z0-9][\\s\\S]*";
+
+        if (inputPass.length() > 20 && inputPassSet.size() >= 10) {
+            return inputPass.matches(upCase) && inputPass.matches(lowCase)
+                   && inputPass.matches(digit) && inputPass.matches(specialChar);
+        }
+        else {
+            return false;
         }
     }
 }
--- src/test/java/edu/hm/hafner/util/StringUtilsTest.java
+++ src/test/java/edu/hm/hafner/util/StringUtilsTest.java
@@ -49,14 +49,42 @@ public class StringUtilsTest {
     }
 
     /**
-     *
-     * FIXME: Document method testStripMethodIllegalArgument
+     * Test if the right exception is thrown by the strip method if a null parameter is passed.
+     * @author Mihail Menev, menev@hm.edu
      */
-    @Test(expected=IllegalArgumentException.class)
+    @Test(expected = IllegalArgumentException.class)
     public void testStripMethodIllegalArgument() {
+        StringUtils.strip("foo", "bar");
         StringUtils.strip(null, "foo");
         StringUtils.strip("foo", null);
         StringUtils.strip(null, null);
+    }
+
+    /**
+     * Test if strong passwords are recognized as such from the isSecure method.
+     * @author Mihail Menev, menev@hm.edu
+     */
+    @Test
+    public void testIsSecure() {
+        assertTrue("This should be a strong password: ", StringUtils.isSecure("seCuR3Pa$SwoRd!!!Ye4H"));
+        assertTrue("This is a test string: ", StringUtils.isSecure("\nA9k§9kl8hgGztrVjjkLpdsss"));
+        assertTrue("YATS: ", StringUtils.isSecure("aqweZtz/iopü sdfgjhkl9mcksd"));
+    }
+
+    /**
+     * Test if unsecure passwords are recognized as such from the method isSecure method.
+     * @author Mihail Menev, menev@hm.edu
+     */
+    @Test
+    public void testIsSecureFalse(){
+        assertFalse("a short password", StringUtils.isSecure("abcd"));
+        assertFalse("An empty password", StringUtils.isSecure(""));
+        assertFalse("A password with only lower case", StringUtils.isSecure("abcdefghijklmnopqrstuvwxyz"));
+        assertFalse("Only UPEER CASE password", StringUtils.isSecure("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+        assertFalse("Uppercase and lowercase", StringUtils.isSecure("abcdeabcdeABcdeabcd3!"));
+        assertFalse("Everything correct but the special char", StringUtils.isSecure("pqrabcd3efghL569KMinS"));
+        assertFalse("Everything correct but the number", StringUtils.isSecure("pqrabcd§efghL%&/KMinS"));
+        assertFalse("Null as a password", StringUtils.isSecure(null));
     }
 
 
