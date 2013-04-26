5488642fe321b52094a5f9fdbf0c8391c0c6ffcd
mmenev <menev@hm.edu>
2013-04-26 15:25:23 +0200

    First simple test of ByteStreams.copy method

--- src/test/java/edu/hm/hafner/util/GuavaByteStreamCopyTest.java
+++ src/test/java/edu/hm/hafner/util/GuavaByteStreamCopyTest.java
@@ -1,5 +1,6 @@
 package edu.hm.hafner.util;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.io.InputStream;
@@ -16,17 +17,27 @@ import org.junit.Test;
  */
 public class GuavaByteStreamCopyTest {
 
+    /**
+     * Test the guava Bytestream.copy method.
+     * @throws Exception if something goes wrong.
+     */
     @Test
     public void testname() throws Exception {
         // Given
         InputStream is = mock(InputStream.class);
         OutputStream os = mock(OutputStream.class);
+        int bufferSize = 4096;
+        int byteToCopy = 6;
+        when(is.read(new byte[bufferSize])).thenReturn(byteToCopy, -1);
+
 
         // When
-        long tmp = ByteStreams.copy(is, os);
+        long result = ByteStreams.copy(is, os);
+
 
         //Then
-        verify(os).close();
+        verify(os).write(new byte[bufferSize], 0, byteToCopy);
+        assertEquals("Expected to write a 6: ", byteToCopy, result);
     }
 
 }
