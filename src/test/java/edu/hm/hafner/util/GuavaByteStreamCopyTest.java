4d3acd73fc3933cd287b733419793ca0d79c3273
mmenev <menev@hm.edu>
2013-04-29 22:33:41 +0200

    Guava  Bytestreams.copy test case implemented.

--- src/test/java/edu/hm/hafner/util/GuavaByteStreamCopyTest.java
+++ src/test/java/edu/hm/hafner/util/GuavaByteStreamCopyTest.java
@@ -3,7 +3,6 @@ package edu.hm.hafner.util;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,9 +14,11 @@ import com.google.common.io.OutputSupplier;
 import org.junit.Test;
 
 /**
- * FIXME: Document type GuavaByteStreamCopyTest.
+ * ClassGuavaByteStreamsCopyTest.
+ * Mockito exercise.
  *
- * @author Ulli Hafner
+ * @author Johann Vierthaler, johann.vierthaler@web.de
+ * @author Mihail Menev, menev@hm.edu
  */
 public class GuavaByteStreamCopyTest {
 
@@ -28,27 +29,26 @@ public class GuavaByteStreamCopyTest {
     @Test
     public void testname() throws Exception {
         // Given
+        // Input- and Outputstream mocks
         InputStream is = mock(ByteArrayInputStream.class);
         OutputSupplier<OutputStream> ous = new OutputSupplier<OutputStream>() {
 
             @Override
             public OutputStream getOutput() throws IOException {
-                //return mock(OutputStream.class);
-                return new BufferedOutputStream(System.out);
+                return mock(OutputStream.class);
             }};
-        byte[] toCopy = "Mockito".getBytes();
-        when(is.read(toCopy)).thenReturn(-1);
 
+        // A byte array that should be copied
+        byte[] toCopy = "Mockito".getBytes();
+        // -1 closes the stream
+        when(is.read(toCopy)).thenReturn(-1);
 
         // When
         is = new ByteArrayInputStream(toCopy);
+        // result holds the number of bytes copied
         long result = ByteStreams.copy(is, ous);
-
 
         //Then
         assertEquals("Expected to write a 6: ", toCopy.length, result);
-
-
     }
-
 }
